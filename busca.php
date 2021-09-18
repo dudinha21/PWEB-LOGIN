e6c0ce1488c4a791f4e0f36a24cd2cbf2646744a
dudinha21 <65730044+dudinha21@users.noreply.github.com>
2021-09-17 21:53:31 -0300

    Add files via upload

---
+++ busca.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'Classes/pessoas.php';
+$p = new pessoa;
+
+
+$nome =  $_POST['nome'];
+$usuario = $_POST['usuario'];
+$email= $_POST['email'];
+$senha = $_POST['senha'];
+
+$p->conectar("recuperacao", "localhost", "root", "");
+if($p->erro == "")
+{
+    $p->listagem();
+}
+else{
+    echo "erro na conexão";
+}
+
+header("location: busca.html");
+
+    
+?>
